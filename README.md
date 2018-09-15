950c61fe81f6f61129a4f748407101729bd54aa9
Remon Lam <remon@containerstack.io>
2018-09-15 11:07:37 +0200

    add files for new master

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rotate between several url's
+
+Enter the sites you want to use @ "sites" rotation speed can be changed it's set at 10000 (10sec)
---
+++ cycler.js
@@ -0,0 +1,21 @@
+var sites = [
+  "http://ups1.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups2.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups3.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups4.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups5.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups6.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups7.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "http://ups8.c-s.io/cgi-bin/apcupsd/upsstats.cgi",
+  "https://api.buienradar.nl/image/1.0/lightningnl",
+  "http://10.10.100.100:8000",
+];
+var currentSite = sites.length;
+
+$(document).ready(function () {
+  var $iframe = $("iframe").attr("src","http://www.google.com");
+  setInterval(function() {
+    (currentSite == 0) ? currentSite = sites.length - 1 : currentSite = currentSite -1;
+    $iframe.attr("src",sites[currentSite]);
+  }, 5000);
+});
